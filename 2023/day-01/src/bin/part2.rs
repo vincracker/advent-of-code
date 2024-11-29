6dd105d6765d36792a55637b1fcec30018985eca
vincracker <vincentchen0930@gmail.com>
2024-11-29 15:47:01 +0800

    finished day1 part 2

--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -1,3 +1,55 @@
 fn main() {
     println!("Part 2");
+    let input = include_str!("./input2.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> String {
+    let output: u32 = input
+        .lines()
+        .map(|line| {
+            let line = line
+                .replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e");
+
+            let mut it = line.chars().filter_map(|character| character.to_digit(10));
+            let first = it.next().expect("Should be a number");
+            let last = it.last();
+
+            dbg!(match last {
+                Some(num) => format!("{first}{num}").parse::<u32>(),
+                None => format!("{first}{first}").parse::<u32>(),
+            })
+            .expect("Should be a number")
+        })
+        .sum();
+
+    output.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = process(
+            "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen",
+        );
+        assert_eq!(result, "281".to_string());
+    }
 }
